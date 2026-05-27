4b8ec20b36118252032cda51b829d6a6eb99fb6a
unknown123-nav <navitakoshy@gmail.com>
2026-05-27 15:48:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 ﻿from flask import Flask, request, jsonify
 
 import numpy as np
+import os
 
 from tensorflow.keras.models import load_model
 
@@ -53,7 +54,15 @@ def predict():
 # =========================
 
 if __name__ == '__main__':
+
+    port = int(
+        os.environ.get(
+            "PORT",
+            5000
+        )
+    )
+
     app.run(
         host='0.0.0.0',
-        port=5000
+        port=port
     )
