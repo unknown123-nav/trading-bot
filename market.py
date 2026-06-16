886024cb85e7bc8f51e587702176752730d4c994
unknown123-nav <navitakoshy@gmail.com>
2026-06-16 11:46:11 +0100

    Update market.py

--- market.py
+++ market.py
@@ -26,14 +26,14 @@ def get_data(symbol, timeframe='1m', limit=50):
         if df.empty:
             return df
 
-        # ✅ SAFE numeric conversion
+        #  SAFE numeric conversion
         numeric_cols = ["open", "high", "low", "close", "vol"]
         df[numeric_cols] = df[numeric_cols].apply(pd.to_numeric, errors="coerce")
 
-        # ✅ ULTRA SAFE TIME FIX (KEY CHANGE)
+        #  ULTRA SAFE TIME FIX (KEY CHANGE)
         df["time"] = pd.to_numeric(df["time"], errors="coerce")
 
-        # ⚠️ CLIP extreme values BEFORE converting
+        # CLIP extreme values BEFORE converting
         df.loc[df["time"] > 2e12, "time"] = None
 
         df["time"] = pd.to_datetime(
@@ -42,15 +42,48 @@ def get_data(symbol, timeframe='1m', limit=50):
             errors="coerce"
         )
 
-        # ✅ remove invalid rows
+        #  remove invalid rows
         df = df.dropna(subset=["time"])
 
-        # ✅ final clean
+        #  final clean
         df = df.sort_values(by="time", ascending=False).reset_index(drop=True)
         df = df[["time", "open", "high", "low", "close", "vol"]]
+
+        df["EMA20"] = (
+            df["close"]
+            .ewm(span=20)
+            .mean()
+        )
+        delta = df["close"].diff()
+        gain = delta.clip(lower=0)
+        loss = -delta.clip(upper=0)
+
+        avg_gain = gain.rolling(14).mean()
+        avg_loss = loss.rolling(14).mean()
+
+        rs = avg_gain / avg_loss
+        df["RSI"] = 100 - (100 / (1 + rs))
+        tr1 = df["high"] - df["low"]
+        tr2 = abs(
+            df["high"] -
+            df["close"].shift()
+        )
+        tr3 = abs(
+            df["low"] -
+            df["close"].shift()
+        )
+        tr = pd.concat(
+            [tr1, tr2, tr3],
+            axis=1
+        ).max(axis=1)
+        df["ATR"] = tr.rolling(14).mean()
+        df["VOLATILITY"] = (
+            (df["high"] - df["low"])
+            / df["close"]
+        ) * 100
 
         return df
 
     except Exception as e:
-        print("❌ Market Data Error:", e)
+        print(" Market Data Error:", e)
         return pd.DataFrame()
