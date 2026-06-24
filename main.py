930ee8509cde9e2976a1b8f0886b34fde063d073
unknown123-nav <navitakoshy@gmail.com>
2026-06-24 21:14:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bot running ✅"
+    return "Bot running "
 
 # =========================================
-# ✅ TELEGRAM ASSISTANT (SAFE LOOP)
+# ✅ TELEGRAM ASSISTANT (SAFE LOOP) 
 # =========================================
 def assistant_loop():
     print("🤖 Assistant Started")
@@ -24,26 +24,40 @@ def assistant_loop():
             from telegram import check_replies
             check_replies()
         except Exception as e:
-            print("❌ Assistant Error:", e)
+            print(" Assistant Error:", e)
 
         time.sleep(2)
 
+ddef weekly_loop():
+
+    print("WEEKLY RETRAINER STARTED")
+
+    try:
+
+        import weekly_scheduler
+
+    except Exception as e:
+
+        print(
+            "Weekly thread error:",
+            e
+        )
 # =========================================
 # ✅ TRADING THREAD
 # =========================================
 def trading_loop():
-    print("🚀 Trading Engine Started")
+    print(" Trading Engine Started")
 
     while True:
         try:
             from bot_engine import run_bot
-            print("✅ bot_engine imported")
+            print("bot_engine imported")
 
-            print("💓 BOT STARTING...")
+            print("BOT STARTING...")
             run_bot()
 
         except Exception as e:
-            print("❌ Trading crashed:", e)
+            print("Trading crashed:", e)
 
             # ✅ restart after crash
             time.sleep(5)
@@ -51,11 +65,15 @@ def trading_loop():
 # =========================================
 # ✅ START THREADS
 # =========================================
-print("✅ MAIN STARTED")
+print("MAIN STARTED")
 
 threading.Thread(target=trading_loop, daemon=True).start()
 threading.Thread(target=assistant_loop, daemon=True).start()
 
+threading.Thread(
+    target=weekly_loop,
+    daemon=True
+).start()
 # ✅ FLASK RUN
 app.run(
     host="0.0.0.0",
