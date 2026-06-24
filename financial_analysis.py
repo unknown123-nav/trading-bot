d22b2f1bda1fd7bc432b30d8ce409424c8b17ae7
unknown123-nav <navitakoshy@gmail.com>
2026-06-25 00:48:25 +0100

    Refactor financial_strength to use DataFrame
    Refactor financial_strength function to accept a DataFrame and return it with a new column for financial strength score.

--- financial_analysis.py
+++ financial_analysis.py
@@ -1,25 +1,13 @@
-def financial_strength(
-    rsi,
-    trend_strength,
-    volatility
-):
+def financial_strength(df):
 
-    score = 0
+    df["FINANCIAL_STRENGTH"] = (
 
-    if 40 <= rsi <= 70:
-        score += 1
+        df["POWER_SCORE"]
 
-    if trend_strength > 0.30:
-        score += 1
+        *
 
-    if volatility > 1:
-        score += 1
+        df["TREND_STRENGTH"]
 
-    if score == 3:
-        return "STRONG"
+    )
 
-    elif score == 2:
-        return "MODERATE"
-
-    else:
-        return "WEAK"
+    return df
