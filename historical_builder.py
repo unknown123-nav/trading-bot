10a25992e2161b1e0e2710e7e7f47e371819cfc3
unknown123-nav <navitakoshy@gmail.com>
2026-06-24 19:58:04 +0100

    Refactor save_historical_signal function parameters

--- historical_builder.py
+++ historical_builder.py
@@ -1,35 +1,132 @@
-columns=[
+from db import get_connection
 
-"time",
-"uk_time",
-"pair",
-"timeframe",
-"price",
-"price_gbp",
+def save_historical_signal(
+    time,
+    uk_time,
+    pair,
+    timeframe,
+    price,
+    price_gbp,
+    EMA20,
+    EMA50,
+    RSI,
+    ATR,
+    NATR,
+    BB_WIDTH,
+    CHAIKIN_VOL,
+    VQI,
+    TREND_STRENGTH,
+    CHANNEL_POSITION,
+    direction,
+    confidence,
+    power_score,
+    financial_strength,
+    signal_class,
+    market_state,
+    frequency_type,
+    candle_type
+):
 
-"EMA20",
-"EMA50",
-"RSI",
-"ATR",
-"NATR",
-"BB_WIDTH",
-"CHAIKIN_VOL",
-"VQI",
-"TREND_STRENGTH",
-"CHANNEL_POSITION",
+    conn = get_connection()
+    cursor = conn.cursor()
 
-"direction",
-"confidence",
+    try:
 
-"power_score",
+        cursor.execute("""
+        INSERT INTO ai_training_dataset(
 
-"financial_strength",
+        time,
+        uk_time,
+        pair,
+        timeframe,
+        price,
+        price_gbp,
 
-"signal_class",
+        EMA20,
+        EMA50,
+        RSI,
+        ATR,
+        NATR,
+        BB_WIDTH,
+        CHAIKIN_VOL,
+        VQI,
+        TREND_STRENGTH,
+        CHANNEL_POSITION,
 
-"market_state",
+        direction,
+        confidence,
 
-"frequency_type",
+        power_score,
 
-"candle_type"
-]
+        financial_strength,
+
+        signal_class,
+
+        market_state,
+
+        frequency_type,
+
+        candle_type,
+
+        target
+
+        )
+
+        VALUES(
+        %s,%s,%s,%s,%s,%s,
+        %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,
+        %s,%s,%s,%s,%s,%s,%s,%s,
+        NULL
+        )
+
+        """,
+
+        (
+
+        time,
+        uk_time,
+        pair,
+        timeframe,
+        price,
+        price_gbp,
+
+        EMA20,
+        EMA50,
+        RSI,
+        ATR,
+        NATR,
+        BB_WIDTH,
+        CHAIKIN_VOL,
+        VQI,
+        TREND_STRENGTH,
+        CHANNEL_POSITION,
+
+        direction,
+        confidence,
+
+        power_score,
+
+        financial_strength,
+
+        signal_class,
+
+        market_state,
+
+        frequency_type,
+
+        candle_type
+
+        ))
+
+        conn.commit()
+
+        print("Historical row saved")
+
+    except Exception as e:
+
+        print("Historical error:",e)
+
+    finally:
+
+        cursor.close()
+        conn.close()
