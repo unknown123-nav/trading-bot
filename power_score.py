8bf1cacb5973ccfbfe69e6e396cca5dbbcdc111e
unknown123-nav <navitakoshy@gmail.com>
2026-06-25 00:47:42 +0100

    Update power_score.py

--- power_score.py
+++ power_score.py
@@ -1,18 +1,36 @@
-def calculate_power_score(
-    confidence,
-    trend_strength,
-    natr,
-    vqi
-):
+import numpy as np
 
-    score = (
-        confidence * 0.50
-        +
-        trend_strength * 30
-        +
-        natr * 10
-        +
-        vqi * 10
+def calculate_power_score(df):
+
+    body = abs(
+        df["close"] -
+        df["open"]
     )
 
-    return round(min(score,100),2)
+    rng = (
+        df["high"] -
+        df["low"]
+    )
+
+    volume_factor = (
+        df["vol"] /
+        (
+            df["vol"]
+            .rolling(20)
+            .mean()
+            + 1e-8
+        )
+    )
+
+    score = (
+        body /
+        (rng + 1e-8)
+    ) * 100
+
+    df["POWER_SCORE"] = (
+        score * 0.70
+        +
+        volume_factor * 30
+    )
+
+    return df
