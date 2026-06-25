dbcf0bd44e1fa6ce2e7293bc74ac812312a012eb
unknown123-nav <navitakoshy@gmail.com>
2026-06-25 01:37:01 +0100

    Update flat_classifier.py

--- flat_classifier.py
+++ flat_classifier.py
@@ -1,17 +1,86 @@
-import numpy as np
+# ==========================================
+# MARKET STATE CLASSIFIER
+# ==========================================
 
 def market_state(df):
 
-    df["MARKET_STATE"] = "NORMAL"
+    latest = df.iloc[-1]
 
-    df.loc[
-        df["TREND_STRENGTH"] > 0.20,
-        "MARKET_STATE"
-    ] = "TRENDING"
+    ema20 = latest["EMA20"]
+    ema50 = latest["EMA50"]
 
-    df.loc[
-        df["BB_WIDTH"] < 0.50,
-        "MARKET_STATE"
-    ] = "RANGING"
+    rsi = latest["RSI"]
+
+    trend = latest["TREND_STRENGTH"]
+
+    natr = latest["NATR"]
+
+    bb = latest["BB_WIDTH"]
+
+    cp = latest["CHANNEL_POSITION"]
+
+    # =====================================
+    # BREAKOUT
+    # =====================================
+
+    if (
+        trend > 2
+        and
+        bb > 1.5
+        and
+        0.80 <= cp <= 1.00
+    ):
+
+        state = "BREAKOUT"
+
+    # =====================================
+    # REVERSAL
+    # =====================================
+
+    elif (
+        rsi > 75
+        or
+        rsi < 25
+    ):
+
+        state = "REVERSAL"
+
+    # =====================================
+    # STRONG TREND
+    # =====================================
+
+    elif (
+        abs(ema20-ema50)/ema50*100 > 1
+        and
+        trend > 1
+    ):
+
+        state = "TRENDING"
+
+    # =====================================
+    # HIGH VOLATILITY
+    # =====================================
+
+    elif natr > 3:
+
+        state = "VOLATILE"
+
+    # =====================================
+    # SIDEWAYS
+    # =====================================
+
+    elif bb < 0.5:
+
+        state = "RANGING"
+
+    # =====================================
+    # NORMAL
+    # =====================================
+
+    else:
+
+        state = "NORMAL"
+
+    df["MARKET_STATE"] = state
 
     return df
